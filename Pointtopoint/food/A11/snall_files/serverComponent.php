d25408c955f3c1db2636bdc300d3732bc2ee7638
Vivorth <60089316+Vivorth@users.noreply.github.com>
2020-02-26 20:38:42 +0700

    update

---
+++ Pointtopoint/food/A11/snall_files/serverComponent.php
@@ -0,0 +1,2 @@
+
+Bootstrapper._serverTime = '2020-02-24 14:23:34'; Bootstrapper._clientIP = '96.9.67.154'; Bootstrapper.insertPageFiles(['https://agility.cnn.com/turner/cnn-prod/code/1ce0b2f61595da9291b80103845c2c3e.js?conditionId0=423209','https://agility.cnn.com/turner/cnn-prod/code/a66573a20cf891d2f62a2a0db4d5f490.js?conditionId0=4839055']);Bootstrapper.setPageSpecificDataDefinitionIds([50215,8909]);
